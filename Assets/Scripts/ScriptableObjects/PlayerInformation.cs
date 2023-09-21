ead37dec64cd1c241c62da83d5736984473a6a6a
clayton <claytonpinter@gmail.com>
2023-09-21 18:30:28 -0500

    PlayerInformation class and Damagable interface

--- Assets/Scripts/ScriptableObjects/PlayerInformation.cs
+++ Assets/Scripts/ScriptableObjects/PlayerInformation.cs
@@ -1,91 +1,91 @@
 using System;
 using UnityEngine;
 
-public class PlayerInformation : MonoBehaviour, IDamagable
+namespace ScriptableObjects
 {
-    /**
-     * The full capacity of the shield
-     */
-    public float ShieldCapacity = 100F;
-
-    /**
-     * The remaining health of the shield
-     */
-    public float ShieldHealth = 100F;
-
-    /**
-     * Controls how much incoming damage ill be absorbed by the shield
-     */
-    public float DamageResistance = 0.25F;
-
-    public float MaxHealth { get; }
-    public float Health { get; set; }
-
-    public void HealDamage(float value)
+    public class PlayerInformation : ScriptableObject, Damagable
     {
-        Health = Mathf.Clamp(Health + value, 0f, MaxHealth);
-    }
-
-    public void TakeDamage(float value)
-    {
-        // Calculate the value that would be absorbed
-        var absorbed = value * DamageResistance;
-
-        // If its trying to absorb more than it has health, set absorbed to its health    
-        if (absorbed > ShieldHealth) absorbed = ShieldHealth;
-
-        // Remove the amount it absorbed
-        DamageShieldHealth(absorbed);
-        // Remove the initial damage, with the absorbed amount removed. 
-        var newValue = Health - value;
-
-        if (newValue <= 0)
+        public PlayerInformation(double maxHealth)
         {
-            Health = 0;
-            Die();
-            return;
+            this.maxHealth = maxHealth;
         }
 
-        Health = newValue;
-    }
+        public double maxHealth { get; }
 
-    /**
-     * Logic for when the player is killed. Called when health reaches zero.
-     */
-    public void Die()
-    {
-        throw new NotImplementedException("Need logic for player death");
-    }
+        public double health { get; set; }
 
-    /**
-     * Adds health to the shield
-     */
-    public void HealShieldHealth(float value)
-    {
-        ShieldHealth = Mathf.Clamp(ShieldHealth + value, 0f, ShieldCapacity);
-    }
+        /** The full capacity of the shield */
+        public double shieldCapacity;
 
-    /**
-     * removes health
-     */
-    public void DamageShieldHealth(float value)
-    {
-        var newValue = ShieldHealth - value;
+        /** The remaining health of the shield */
+        public double shieldHealth;
 
-        if (newValue <= 0)
+        /** Controls how much incoming damage ill be absorbed by the shield */
+        private double damageResistance;
+
+        public void removeHealth(double value)
         {
-            shieldsEmptied();
-            newValue = 0;
+            // Calculate the value that would be absorbed
+            double absorbed = value * damageResistance;
+            
+            // If its trying to absorb more than it has health, set absorbed to its health    
+            if (absorbed > shieldHealth)
+            { 
+                absorbed = shieldHealth;
+            }
+            
+            // Remove the amount it absorbed
+            removeShieldHealth(absorbed);
+            // Remove the initial damage, with the absorbed amount removed. 
+            double newValue = health - value;
+        
+            if (newValue <= 0)
+            {
+                health = 0;
+                kill();
+                return;
+            }
+
+            health = newValue;
+        }
+        
+        /** Adds health to the shield */
+        public void addShieldHealth(double value)
+        {
+            double newValue = shieldHealth + value;
+        
+            if (newValue > shieldCapacity)
+            {
+                newValue = shieldCapacity;
+            }
+
+            shieldHealth = newValue;
+        }
+        
+        /** removes health */
+        public void removeShieldHealth(double value)
+        {
+            double newValue = shieldHealth - value;
+        
+            if (newValue <= 0)
+            {
+                shieldsEmptied();
+                newValue = 0;
+            }
+
+            shieldHealth = newValue;
         }
 
-        ShieldHealth = newValue;
-    }
-
-    /**
-     * Logic for when the player shields have been emptied.
-     */
-    public void shieldsEmptied()
-    {
-        throw new NotImplementedException("Need logic for when the shields are empty");
+        /** Logic for when the player is killed. Called when health reaches zero. */
+        public void kill()
+        {
+            throw new NotImplementedException("Need logic for player death");
+        }
+        
+        /** Logic for when the player shields have been emptied. */
+        public void shieldsEmptied()
+        {
+            throw new NotImplementedException("Need logic for when the shields are empty");
+        }
     }
 }

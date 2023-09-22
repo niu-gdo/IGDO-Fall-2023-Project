688660f915237c1bbac238e846e629d8bdf8f6e0
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2023-09-22 15:16:59 -0500

    Finish implementation of projectile.
    The projectile can be fired, and it collides with objects correctly. Some notes; first, the bullets cluster together if the player fires in the direction they are moving. The current workaround is to make projectiles not check for collision with each other; another solution may be preferable. Second, several changes were made to the player controller script in order to instantiate the bullets in a way that did not interfere with the player or cause the bullets to cease existing immediately. Other solutions may be preferable.

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -5,9 +5,13 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    public GameObject weaponProjectile;
+    [SerializeField] private GameObject _weaponProjectile;
     private Rigidbody2D _rb;
     private Vector2 _movement;
+    private int _fireRate = 0;
+    private Vector2 _projectileSpawnModifier;
+    private Camera _cam;
+    private CapsuleCollider2D _playerBoundingBox;
 
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
@@ -15,20 +19,40 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        //Initialize access to the main camera and player collision
+        _cam = Camera.main;
+        _playerBoundingBox = GetComponent<CapsuleCollider2D>();
+
         _rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         _rb.AddForce(_movement * _movementSpeed * Time.deltaTime);
-
-        // Check if the player is firing a weapon
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(weaponProjectile, _rb.position, Quaternion.identity);
-        }
     }
 
+
+    private void Update()
+    {
+        // Check if the player has fired a weapon recently
+        if (_fireRate == 0)
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                //Get values necessary to spawn projectiles outside of player collision
+                _projectileSpawnModifier = Input.mousePosition - _cam.WorldToScreenPoint(_rb.position);
+                _projectileSpawnModifier.Normalize();
+                _projectileSpawnModifier = _projectileSpawnModifier * _playerBoundingBox.size;
+
+                Instantiate(_weaponProjectile, _rb.position + _projectileSpawnModifier, Quaternion.identity);
+                _fireRate = 30;
+            }
+        }
+        else
+        {
+            _fireRate = _fireRate - 1;
+        }
+    }
 
     public void OnMove(InputValue value)
     {
--- Assets/Scripts/Controllers/projectileController.cs
+++ Assets/Scripts/Controllers/projectileController.cs
@@ -5,25 +5,33 @@ using UnityEngine.InputSystem;
 
 public class projectileController : MonoBehaviour
 {
-    public CircleCollider2D collisionTracker;
-    public Vector2 bulletDirection;
-    public float bulletSpeed;
-    
+    private Rigidbody2D _projectileCollision;
+    private Rigidbody2D _playerPhysicsBody;
+    private Vector2 _bulletDirection;
+    private float _bulletSpeed = 800f;
+    private Camera _cam;
+    private GameObject _playerCharacter;
     
     void Awake ()
     {
-        
+        //Get position and physics components of projectile; initialize camera variable for converting world to screen coordinates.
+        _projectileCollision = GetComponent<Rigidbody2D>();
+        _cam = Camera.main;
+
+        //Get velocity of player character.
+        _playerCharacter = GameObject.Find("Player");
+        _playerPhysicsBody = _playerCharacter.GetComponent<Rigidbody2D>();
+
+        //Get the direction of the velocity vector of the bullet.
+        _bulletDirection = Input.mousePosition - _cam.WorldToScreenPoint(_projectileCollision.position);
+        _bulletDirection.Normalize();
+
+        _projectileCollision.AddForce(_bulletDirection * _bulletSpeed + _playerPhysicsBody.velocity);
     }
     
     
-    void Update ()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Shoot();
-    }
-    
-    
-    void Shoot ()
-    {
-        
+        Destroy(gameObject);
     }
 }

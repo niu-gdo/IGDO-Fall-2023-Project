4a5d5239bb1927b286a78cdb72c87839a6b878de
iZoomz <kingzette11@gmail.com>
2023-09-23 00:47:17 -0600

    Fixed infinite jump

--- Assets/Scripts/Controllers/CharacterController-DESKTOP-7IQNCQ4.cs
+++ Assets/Scripts/Controllers/CharacterController-DESKTOP-7IQNCQ4.cs
@@ -7,13 +7,16 @@ public class CharacterController : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private Vector2 _movement;
+    private CapsuleCollider2D _cc;
 
+    [SerializeField] private LayerMask groundMask;
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
 
     // Start is called before the first frame update
     void Awake()
     {
+        _cc = GetComponent<CapsuleCollider2D>();
         _rb = GetComponent<Rigidbody2D>();
     }
 
@@ -28,8 +31,15 @@ public class CharacterController : MonoBehaviour
         _movement = value.Get<Vector2>();
     }
 
+    public bool onGround()
+    {   
+        RaycastHit2D hit = Physics2D.Raycast(_cc.bounds.center, Vector2.down, _cc.bounds.extents.y + 0.1f,groundMask);
+        return hit.collider != null;
+    }
     public void OnJump(InputValue value)
     {
-        _rb.AddForce(Vector2.up * _jumpForce);
+        if (onGround()){
+            _rb.AddForce(Vector2.up * _jumpForce);
+        }
     }
 }

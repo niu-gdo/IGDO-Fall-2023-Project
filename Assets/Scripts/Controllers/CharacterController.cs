f2ccdadd99df9c3fef278b616a04085fe5d23699
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2023-09-28 16:35:32 -0500

    Make requested corrections to weapon firing.
    Made a duplicate DeveloperTemplate, restored the original.
    
    Added line comments.
    
    Altered the _fireRate logic so that it works off game time, rather than frames.
    
    Changed the input logic to the new InputSystem.
    
    Changed the name of projectileController to PascalCase.
    
    The player now controls the weapon projectile firing direction and magnitude.
    
    Removed unnecessary using statements from projectileController.
    
    Despawn projectiles after 3 seconds.

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -5,13 +5,17 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private GameObject _weaponProjectile;
+    [SerializeField] private GameObject _weaponProjectile; //Basic projectile prefab, spawned when player fires.
     private Rigidbody2D _rb;
     private Vector2 _movement;
-    private int _fireRate = 0;
-    private Vector2 _projectileSpawnModifier;
-    private Camera _cam;
-    private CapsuleCollider2D _playerBoundingBox;
+    private float _fireRate = 0; //Used to control weapon firing speed.
+    private Vector2 _projectileSpawnModifier; //The offset for spawning projectiles, and the direction they should move in after being fired.
+    private Camera _cam; //Gives access to the main game camera
+    private CapsuleCollider2D _playerBoundingBox; //Bounding box for the collision detection of the player.
+    private bool _isTriggerHeld = false; //Tracks if the left mouse button is being held.
+    private Rigidbody2D _projectileCollision; //Collision detection for the projectile fired by the current weapon. Used to move projectiles after firing.
+    private float _bulletSpeed = 800f; //Speed of projectiles fired by current weapon; 800 is default value.
+    private int _weaponHeld = 0; //Weapon currently in use by the player.
 
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
@@ -31,27 +35,43 @@ public class CharacterController : MonoBehaviour
         _rb.AddForce(_movement * _movementSpeed * Time.deltaTime);
     }
 
-
-    private void Update()
+    private void TryFire()
     {
-        // Check if the player has fired a weapon recently
-        if (_fireRate == 0)
+        //Handle the logic to determine what weapon to fire.
+        switch(_weaponHeld)
         {
-            if(Input.GetButton("Fire1"))
-            {
+            default:
                 //Get values necessary to spawn projectiles outside of player collision
                 _projectileSpawnModifier = Input.mousePosition - _cam.WorldToScreenPoint(_rb.position);
                 _projectileSpawnModifier.Normalize();
                 _projectileSpawnModifier = _projectileSpawnModifier * _playerBoundingBox.size;
 
-                Instantiate(_weaponProjectile, _rb.position + _projectileSpawnModifier, Quaternion.identity);
-                _fireRate = 30;
-            }
+                //Spawn the weapon projectile.
+                GameObject newProjectile = Instantiate(_weaponProjectile, _rb.position + _projectileSpawnModifier, Quaternion.identity);
+
+                //Make the projectile move in the desired direction.
+                _projectileSpawnModifier.Normalize();
+                _projectileCollision = newProjectile.GetComponent<Rigidbody2D>();
+                _projectileCollision.AddForce(_projectileSpawnModifier * _bulletSpeed);
+            
+                //Handle the firing rate.
+                _fireRate = Time.time + 0.01f;
+                break;
         }
-        else
+    }
+
+    void Update()
+    {
+        if(_isTriggerHeld && Time.time > _fireRate)
         {
-            _fireRate = _fireRate - 1;
+            TryFire();
         }
+    }
+
+    private void OnShoot(InputValue value)
+    {
+        //If the button is pressed, flip to true. Once the button is released, flip to false.
+        _isTriggerHeld = !_isTriggerHeld;
     }
 
     public void OnMove(InputValue value)
--- Assets/Scripts/Controllers/projectileController.cs
+++ Assets/Scripts/Controllers/projectileController.cs
@@ -1,36 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class projectileController : MonoBehaviour
+public class ProjectileController : MonoBehaviour
 {
-    private Rigidbody2D _projectileCollision;
-    private Rigidbody2D _playerPhysicsBody;
-    private Vector2 _bulletDirection;
-    private float _bulletSpeed = 800f;
-    private Camera _cam;
-    private GameObject _playerCharacter;
+    private float despawnTimer; //When to despawn the projectiles after being fired.
     
-    void Awake ()
+    void Awake () //Set the despawn timer.
     {
-        //Get position and physics components of projectile; initialize camera variable for converting world to screen coordinates.
-        _projectileCollision = GetComponent<Rigidbody2D>();
-        _cam = Camera.main;
-
-        //Get velocity of player character.
-        _playerCharacter = GameObject.Find("Player");
-        _playerPhysicsBody = _playerCharacter.GetComponent<Rigidbody2D>();
-
-        //Get the direction of the velocity vector of the bullet.
-        _bulletDirection = Input.mousePosition - _cam.WorldToScreenPoint(_projectileCollision.position);
-        _bulletDirection.Normalize();
-
-        _projectileCollision.AddForce(_bulletDirection * _bulletSpeed + _playerPhysicsBody.velocity);
+        despawnTimer = Time.time + 3;
     }
+
+    void Update() //Destroy the projectiles after the despawn timer runs out.
+    {
+        if(Time.time > despawnTimer)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     
-    
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) //Destroy the object on collision.
     {
         Destroy(gameObject);
     }

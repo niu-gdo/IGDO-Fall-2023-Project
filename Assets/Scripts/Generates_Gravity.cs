5e5a20d388979f171c88dec912230856dfcedee8
Nmerryman <nmerryman9001@gmail.com>
2023-09-29 21:30:44 -0500

    Changed the system to work with a GravVec struct to easily pass a bit more info than just a force vector

--- Assets/Scripts/Generates_Gravity.cs
+++ Assets/Scripts/Generates_Gravity.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
 
 public class Generates_Gravity : MonoBehaviour
 {
-    [SerializeField] private float _gravStrength = 20;
     private Generates_Gravity _self;
+    [SerializeField] private float _gravStrength = 20;
+    [SerializeField] private int _gravType = 0;
+    [SerializeField] private int _gravLevel = 0;
+    
     
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,9 +28,29 @@ public class Generates_Gravity : MonoBehaviour
         
     }
 
-    public Vector3 CalcGrav(Rigidbody2D body)
+    public GravVec CalcGrav(Rigidbody2D body)
     {
-        return Vector3.down * _gravStrength * body.mass;
+        GravVec result;
+        switch (_gravType)
+        {
+            case 0:  // Default down gravaty
+                result = new GravVec(Vector2.down * _gravStrength * body.mass, _gravLevel);
+                break;
+            case 1:  // based on 
+                var offset = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) - body.position;
+                var dist = offset.sqrMagnitude;
+                var force = _gravStrength * body.mass / dist;
+                var angle = offset.y / offset.x;
+                var forceVec = new Vector2(math.cos(angle) * force, math.sin(angle) * force);
+
+                result = new GravVec(forceVec, _gravLevel);
+                break;
+            default:
+                result = new GravVec(Vector2.zero, _gravLevel);
+                break;
+        }
+
+        return result;
     }
 
     private void OnTriggerExit2D(Collider2D other)

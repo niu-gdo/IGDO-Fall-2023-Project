0b6b2fa912819a841615605f4fb23142f741faa5
Nmerryman <nmerryman9001@gmail.com>
2023-09-29 22:22:55 -0500

    Made Gravity type and level use public and a choosable enum.

--- Assets/Scripts/Applies_Gravity.cs
+++ Assets/Scripts/Applies_Gravity.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public enum GravLevelUse
+{
+    Sum,
+    Highest
+}
 
 public class Applies_Gravity : MonoBehaviour
 {
@@ -9,6 +16,7 @@ public class Applies_Gravity : MonoBehaviour
     private Rigidbody2D _rb;   
     // Keeps track of all gravity fields that we are currently touching
     private List<Generates_Gravity> _appliedForces = new List<Generates_Gravity>();
+    [SerializeField] private GravLevelUse _gravLevelUse = GravLevelUse.Sum;
 
     public void Set(ref Generates_Gravity grav)
     // Insert interacting field to list if it's new (Avoids double counting n stuff)
@@ -45,10 +53,34 @@ public class Applies_Gravity : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Apply each force to the current body
-        foreach (var force in _appliedForces)
+
+        switch (_gravLevelUse)
         {
-            _rb.AddForce(force.CalcGrav(_rb).Force);
+            case GravLevelUse.Sum:
+                foreach (var force in _appliedForces)
+                {
+                    _rb.AddForce(force.CalcGrav(_rb).Force);
+                }
+                break;
+            case GravLevelUse.Highest:
+                var highest = -999;
+                var vecSum = Vector2.zero;
+                foreach (var force in _appliedForces)
+                {
+                    var res = force.CalcGrav(_rb);
+                    if (res.Level > highest)
+                    {
+                        highest = res.Level;
+                        vecSum = res.Force;
+                    }
+                    else if (res.Level == highest)
+                    {
+                        vecSum += res.Force;
+                    }
+                }
+                break;
+            default:
+                break;
         }
         
     }

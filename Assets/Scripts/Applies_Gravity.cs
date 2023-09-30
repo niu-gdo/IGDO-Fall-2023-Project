ae3ac4448d0b02c839e4b4d3d69e7b7c1f87fe6a
Nmerryman <nmerryman9001@gmail.com>
2023-09-30 00:13:43 -0500

    Fixed a bug in realistic grav mode and added a bunch of documentation.

--- Assets/Scripts/Applies_Gravity.cs
+++ Assets/Scripts/Applies_Gravity.cs
@@ -1,15 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 
+/// <summary>
+/// What method to use to calculate net gravity forces
+/// </summary>
 public enum GravLevelUse
 {
-    Sum,
-    Highest
+    Sum,        // Just sum all applied forces
+    Highest     // Sum all forces with the highest given priority
 }
 
+/// <summary>
+/// Any entity that has this receiver can register hooks from Generators to ask for gravity calculations.
+/// </summary>
 public class Applies_Gravity : MonoBehaviour
 {
     
@@ -18,9 +22,14 @@ public class Applies_Gravity : MonoBehaviour
     private List<Generates_Gravity> _appliedForces = new List<Generates_Gravity>();
     [SerializeField] private GravLevelUse _gravLevelUse = GravLevelUse.Sum;
 
+    /// <summary>
+    /// Try to insert Generator reference into list of forces
+    /// </summary>
+    /// <param name="grav">Reference of the Generator connected to the gravity field collider</param>
     public void Set(ref Generates_Gravity grav)
     // Insert interacting field to list if it's new (Avoids double counting n stuff)
     {
+        // Make sure it is new
         var newSource = true;
         foreach (var item in _appliedForces)
         {
@@ -34,41 +43,47 @@ public class Applies_Gravity : MonoBehaviour
         {
             _appliedForces.Add(grav);
         }
-        Debug.Log("Added " + grav.GetInstanceID());
     }
 
+    /// <summary>
+    /// Remove Generator when out of range (detected by exiting collider)
+    /// </summary>
+    /// <param name="grav">Reference of Generator to be removed</param>
     public void Reset(Generates_Gravity grav)
     // Removes Gravity field from tracking list
     {
         _appliedForces.RemoveAll(i => i.GetInstanceID() == grav.GetInstanceID());
-        Debug.Log("Removed " + grav.GetInstanceID());
     }
     
     
-    // Start is called before the first frame update
+    /// <summary>
+    /// Load our rigidbody which is needed to apply forces to
+    /// </summary>
     void Start() 
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    /// <summary>
+    /// Applies the gravity forces on each step
+    /// </summary>
     void FixedUpdate()
     {
-
+        // Check how to interpret the level
         switch (_gravLevelUse)
         {
-            case GravLevelUse.Sum:
-                foreach (var force in _appliedForces)
-                {
+            case GravLevelUse.Sum:  // Just add up all forces we calculate
+                foreach (var force in _appliedForces) {
                     _rb.AddForce(force.CalcGrav(_rb).Force);
                 }
                 break;
-            case GravLevelUse.Highest:
+            case GravLevelUse.Highest:  // Sum up all forces of current highest level
                 var highest = -999;
                 var vecSum = Vector2.zero;
                 foreach (var force in _appliedForces)
                 {
                     var res = force.CalcGrav(_rb);
-                    if (res.Level > highest)
+                    if (res.Level > highest)  // reset sum if new highest found
                     {
                         highest = res.Level;
                         vecSum = res.Force;
@@ -78,6 +93,7 @@ public class Applies_Gravity : MonoBehaviour
                         vecSum += res.Force;
                     }
                 }
+                _rb.AddForce(vecSum);
                 break;
             default:
                 break;
--- Assets/Scripts/Generates_Gravity.cs
+++ Assets/Scripts/Generates_Gravity.cs
@@ -1,27 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
 
+/// <summary>
+/// An enum of options on what calculation to use find the gravity force
+/// </summary>
 public enum GravTypes
 {
-    Linear,
-    Realistic
+    Linear,  // Make it go down
+    Realistic  // Pull to center
 }
 
+/// <summary>
+/// Any entity with this component uses it's attached collider2d to hook itself into gravity appliers to provide a gravity influence
+/// </summary>
 public class Generates_Gravity : MonoBehaviour
 {
-    private Generates_Gravity _self;
-    [SerializeField] private float _gravStrength = 20;
-    [SerializeField] private GravTypes _gravType = GravTypes.Linear;
-    [SerializeField] private int _gravPriority = 0;
+    private Generates_Gravity _self;  // Generating a mutable reference to self for passing
+    [SerializeField] private float _gravStrength = 20;  // The strength of the gravity force
+    [SerializeField] private GravTypes _gravType = GravTypes.Linear;  // What mode is used to calculate gravity force
+    [SerializeField] private float _gravAngle = 3 * math.PI / 2;  // Change the angle of the linear gravity force (allows angled gravity)
+    [SerializeField] public int _gravPriority = 0;  // How important the field is (Applier has a setting to ignore lower levels)
+    [SerializeField] private float _maxForce = 30;  // To stop insanely high forces when getting close to origin
     
-    
+    /// <summary>
+    /// Try to insert this collider into Applier if other has component
+    /// </summary>
+    /// <param name="other">The other entity that interacted with our collider</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
         // GameObject temp = other.gameObject;
@@ -32,20 +37,43 @@ public class Generates_Gravity : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Remove self from the other entity when it leaves our collider range
+    /// </summary>
+    /// <param name="other">The other entity that interacted with our collider</param>
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Applies_Gravity>(out Applies_Gravity thing))
+        {
+            thing.Reset(_self);
+        }
+    }
+
+    /// <summary>
+    /// Uses known self information and passed rigidbody information to calculate the gravity force that will be applied to the other body
+    /// </summary>
+    /// <param name="body">The entity that is requesting a gravity force calculation</param>
+    /// <returns>The amount of force applied from this field. Also includes the force priority</returns>
     public GravVec CalcGrav(Rigidbody2D body)
     {
         GravVec result;
         switch (_gravType)
         {
-            case GravTypes.Linear:  // Default down gravaty
-                result = new GravVec(Vector2.down * _gravStrength * body.mass, _gravPriority);
+            case GravTypes.Linear:  // Gravity simply forces in a single direction regardless of location
+                var linForce = _gravStrength * body.mass;
+                // Sin and cos are used to split force into x and y components
+                var linVec = new Vector2(linForce * math.cos(_gravAngle), linForce * math.sin(_gravAngle));
+                result = new GravVec(linVec, _gravPriority);
                 break;
-            case GravTypes.Realistic:  // based on 
+            case GravTypes.Realistic:  // Newton gravity. Higher forces to center of mass
                 var offset = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) - body.position;
-                var dist = offset.sqrMagnitude;
-                var force = _gravStrength * body.mass / dist;
-                var angle = offset.y / offset.x;
-                var forceVec = new Vector2(math.cos(angle) * force, math.sin(angle) * force);
+                var dist = (float)math.max(offset.sqrMagnitude, .001);  // Cap closest distance to avoid insanely high force/div by 0
+                var force = math.min(_gravStrength * body.mass / dist, _maxForce);
+                //  https://math.stackexchange.com/questions/1327253/how-do-we-find-out-angle-from-x-y-coordinates
+                var denom = (float)math.max(math.sqrt(math.pow(offset.x, 2) + math.pow(offset.y, 2)), .000000001);  // Max because I want to avoid div by 0
+                var angleX = offset.x / denom;
+                var angleY = offset.y / denom;
+                var forceVec = new Vector2(angleX * force, angleY * force);
 
                 result = new GravVec(forceVec, _gravPriority);
                 break;
@@ -57,15 +85,9 @@ public class Generates_Gravity : MonoBehaviour
         return result;
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.TryGetComponent<Applies_Gravity>(out Applies_Gravity thing))
-        {
-            thing.Reset(_self);
-        }
-    }
-
-    // Start is called before the first frame update
+     /// <summary>
+     /// Field just needs to grab a ref of itself for hooking purposes.
+     /// </summary>
     void Start()
     {
         _self = this;

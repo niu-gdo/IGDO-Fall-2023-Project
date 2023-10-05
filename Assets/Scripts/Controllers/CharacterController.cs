9e00de37143b8b378e6370f9a83981eb9e80e128
james_a_stephans <130923807+KetsuPami@users.noreply.github.com>
2023-10-05 18:46:43 -0500

    Implement object pooling for default player projectiles.
    The default player weapon firing logic has been updated to use object pooling instead of instantiating new projectiles. In addition, the firing rate has been slowed to 15 per second instead of 100 per second.

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Pool;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private GameObject _weaponProjectile; //Basic projectile prefab, spawned when player fires.
     private Rigidbody2D _rb;
     private Vector2 _movement;
     private float _fireRate = 0; //Used to control weapon firing speed.
@@ -16,9 +16,16 @@ public class CharacterController : MonoBehaviour
     private Rigidbody2D _projectileCollision; //Collision detection for the projectile fired by the current weapon. Used to move projectiles after firing.
     private float _bulletSpeed = 800f; //Speed of projectiles fired by current weapon; 800 is default value.
     private int _weaponHeld = 0; //Weapon currently in use by the player.
+    private IObjectPool<ProjectileController> projectilePool; // stack-based ObjectPool for projectiles.
 
+    [SerializeField] private GameObject defaultProjectile;
+    [SerializeField] private bool collectionCheck = true; //Throw an exception if we try to return an existing projectile, already in the pool
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
+
+    // extra options to control the pool capacity and maximum size
+    [SerializeField] private int defaultCapacity = 15;
+    [SerializeField] private int maxSize = 15;
 
     // Start is called before the first frame update
     void Awake()
@@ -28,6 +35,10 @@ public class CharacterController : MonoBehaviour
         _playerBoundingBox = GetComponent<CapsuleCollider2D>();
 
         _rb = GetComponent<Rigidbody2D>();
+
+        projectilePool = new ObjectPool<ProjectileController>(CreateProjectile,
+                OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject,
+                collectionCheck, defaultCapacity, maxSize); //Instantiate the projectile pool.
     }
 
     private void FixedUpdate()
@@ -41,21 +52,30 @@ public class CharacterController : MonoBehaviour
         switch(_weaponHeld)
         {
             default:
+
                 //Get values necessary to spawn projectiles outside of player collision
                 _projectileSpawnModifier = Input.mousePosition - _cam.WorldToScreenPoint(_rb.position);
                 _projectileSpawnModifier.Normalize();
                 _projectileSpawnModifier = _projectileSpawnModifier * _playerBoundingBox.size;
 
-                //Spawn the weapon projectile.
-                GameObject newProjectile = Instantiate(_weaponProjectile, _rb.position + _projectileSpawnModifier, Quaternion.identity);
+                //Retrieve a pooled weapon projectile.
+                ProjectileController bulletObject = projectilePool.Get();
 
-                //Make the projectile move in the desired direction.
-                _projectileSpawnModifier.Normalize();
-                _projectileCollision = newProjectile.GetComponent<Rigidbody2D>();
-                _projectileCollision.AddForce(_projectileSpawnModifier * _bulletSpeed);
+                if (bulletObject == null)
+                    return;
+                if(projectilePool != null)
+                {               
+                    bulletObject.transform.SetPositionAndRotation(_rb.position + _projectileSpawnModifier, Quaternion.identity);
+
+                    //Make the projectile move in the desired direction.
+                    _projectileSpawnModifier.Normalize();
+                    bulletObject.GetComponent<Rigidbody2D>().AddForce(_projectileSpawnModifier * _bulletSpeed);
+
+                    bulletObject.Deactivate();
+                }
             
                 //Handle the firing rate.
-                _fireRate = Time.time + 0.01f;
+                _fireRate = Time.time + 0.07f;
                 break;
         }
     }
@@ -82,5 +102,31 @@ public class CharacterController : MonoBehaviour
     public void OnJump(InputValue value)
     {
         _rb.AddForce(Vector2.up * _jumpForce);
+    }
+
+    // invoked when creating an item to populate the object pool
+    private ProjectileController CreateProjectile()
+    {
+        ProjectileController projectileInstance = Instantiate(defaultProjectile).GetComponent<ProjectileController>();
+        projectileInstance.ObjectPool = projectilePool;
+        return projectileInstance;
+    }
+
+    // invoked when returning an item to the object pool
+    private void OnReleaseToPool(ProjectileController pooledObject)
+    {
+        pooledObject.gameObject.SetActive(false);
+    }
+
+    // invoked when retrieving the next item from the object pool
+    private void OnGetFromPool(ProjectileController pooledObject)
+    {
+        pooledObject.gameObject.SetActive(true);
+    }
+
+    // invoked when we exceed the maximum number of pooled items (i.e. destroy the pooled object)
+    private void OnDestroyPooledObject(ProjectileController pooledObject)
+    {
+        Destroy(pooledObject.gameObject);
     }
 }

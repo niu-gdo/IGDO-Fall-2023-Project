bf8536ea8c76c63450b14597616fa66358ee93d8
Adam <adamsauce99@gmail.com>
2023-10-18 00:21:06 -0500

    Flushed out PIckup and Drop system, Added approprate Methods to CharacterController

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -25,9 +25,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
 
-    // extra options to control the pool capacity and maximum size
-    [SerializeField] private int defaultCapacity = 15;
-    [SerializeField] private int maxSize = 15;
+    [SerializeField] private Weapon _currentWeapon;
+    [SerializeField] private GameObject _weaponView;
 
     // Start is called before the first frame update
     void Awake()
@@ -48,57 +47,17 @@ public class CharacterController : MonoBehaviour
         _rb.AddForce(_movement * _movementSpeed * Time.deltaTime);
     }
 
-    private void TryFire()
-    {
-        //Handle the logic to determine what weapon to fire.
-        switch(_weaponHeld)
-        {
-            default:
-
-                //Get values necessary to spawn projectiles outside of player collision
-                _projectileSpawnModifier = Input.mousePosition - _cam.WorldToScreenPoint(_rb.position);
-                _projectileSpawnModifier.Normalize();
-                _projectileSpawnModifier = _projectileSpawnModifier * _playerBoundingBox.size;
-
-                //Retrieve a pooled weapon projectile.
-                ProjectileController bulletObject = projectilePool.Get();
-
-                if (bulletObject == null)
-                    return;
-                if(projectilePool != null)
-                {               
-                    bulletObject.transform.SetPositionAndRotation(_rb.position + _projectileSpawnModifier, Quaternion.identity);
-
-                    //Make the projectile move in the desired direction.
-                    _projectileSpawnModifier.Normalize();
-                    bulletObject.GetComponent<Rigidbody2D>().AddForce(_projectileSpawnModifier * _bulletSpeed);
-
-                    bulletObject.Deactivate();
-                }
-            
-                //Handle the firing rate.
-                _fireRate = Time.time + 0.07f;
-                break;
-        }
-    }
-
-    void Update()
-    {
-        if(_isTriggerHeld && Time.time > _fireRate)
-        {
-            TryFire();
-        }
-    }
-
-    private void OnShoot(InputValue value)
-    {
-        //If the button is pressed, flip to true. Once the button is released, flip to false.
-        _isTriggerHeld = !_isTriggerHeld;
-    }
-
     public void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
+
+        if(value.Get<Vector2>() == Vector2.right)
+        {
+            transform.localScale = new(1, 1);
+        }else if(value.Get<Vector2>() == Vector2.left)
+        {
+            transform.localScale = new(-1, 1);
+        }
     }
 
     public void OnJump(InputValue value)
@@ -106,29 +65,69 @@ public class CharacterController : MonoBehaviour
         _rb.AddForce(Vector2.up * _jumpForce);
     }
 
-    // invoked when creating an item to populate the object pool
-    private ProjectileController CreateProjectile()
+    public void OnInteract(InputValue value)
     {
-        ProjectileController projectileInstance = Instantiate(defaultProjectile).GetComponent<ProjectileController>();
-        projectileInstance.ObjectPool = projectilePool;
-        return projectileInstance;
+        // Use a raycast to check if a pickup is in front of the player
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right,.5f);
+
+        //if pickup is in front of player, Pickup the wepon
+        if (hit.collider != null 
+            && hit.collider.GetComponent<PickupObject>() != null)
+        {
+            PickupWeapon(hit.collider.gameObject.GetComponent<PickupObject>());
+        }
     }
 
-    // invoked when returning an item to the object pool
-    private void OnReleaseToPool(ProjectileController pooledObject)
+    public void OnDrop(InputValue value)
     {
-        pooledObject.gameObject.SetActive(false);
+        //make weapon pickup in world
+        MakeWeaponPickup();
+
+        //remove weapon from being equiped
+        _currentWeapon = null;
+
+        //remove player sprite of weapon
+        Destroy(_weaponView);
+
     }
 
-    // invoked when retrieving the next item from the object pool
-    private void OnGetFromPool(ProjectileController pooledObject)
+    public void PickupWeapon(PickupObject obj)
     {
-        pooledObject.gameObject.SetActive(true);
+        //equip weapon in player
+        _currentWeapon = obj.wepaon;
+        // create player sprite of wepaon
+        _weaponView = new GameObject(
+            _currentWeapon.name,
+            typeof(SpriteRenderer)
+        );
+        //set player weapon to my a child, and display
+        _weaponView.transform.SetParent(this.transform);
+        _weaponView.transform.localPosition = new Vector2(0.7f, .1f);
+        _weaponView.GetComponent<SpriteRenderer>().sprite = _currentWeapon._playerSprite;
+
+        //remove world object
+        Destroy(obj.gameObject);
     }
 
-    // invoked when we exceed the maximum number of pooled items (i.e. destroy the pooled object)
-    private void OnDestroyPooledObject(ProjectileController pooledObject)
+    private void MakeWeaponPickup()
     {
-        Destroy(pooledObject.gameObject);
+        //create GameObject
+        GameObject pickup = new GameObject(
+            _currentWeapon.name,
+            typeof(BoxCollider2D),
+            typeof(SpriteRenderer),
+            typeof(PickupObject)
+        );
+        //set position in world
+        pickup.transform.position = new Vector2(
+            transform.position.x + 1,
+            transform.position.y
+         );
+
+        //setup function stuffs
+        pickup.GetComponent<PickupObject>().wepaon = _currentWeapon;
+        pickup.GetComponent<BoxCollider2D>().isTrigger = true;
+        pickup.GetComponent<SpriteRenderer>().sprite = _currentWeapon._Worldsprite;
     }
+
 }

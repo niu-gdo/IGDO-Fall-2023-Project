d830e5ff177fc45f299ce9ec2d760f7e0b34e97a
JakeRogers025@gmail.com <Jakerogers025@gmail.com>
2023-11-03 18:58:39 -0500

    create public methods to access input

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using System;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,34 +9,91 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     public Action OnJumpInput;
-    public Action<bool> OnTriggerHeld;  // Propagates if the shoot button is held
+    public Action OnShootInput;
+
     public float HorizontalInput { get => _movement.x; }
 
-    public CharacterItemHandler _characterItemHandler
-    {
-        get
-        {
-            return GetComponent<CharacterItemHandler>();
-        }
-        private set { }
-    }
+    [SerializeField] private GameObject _weaponProjectile; //Basic projectile prefab, spawned when player fires.
+    private Rigidbody2D _rb;
+    
+     
 
     private Vector2 _movement;
+    private float _fireRate = 0; //Used to control weapon firing speed.
+    private Vector2 _projectileSpawnModifier; //The offset for spawning projectiles, and the direction they should move in after being fired.
+    private Camera _cam; //Gives access to the main game camera
+    private CapsuleCollider2D _playerBoundingBox; //Bounding box for the collision detection of the player.
+    private bool _isTriggerHeld = false; //Tracks if the left mouse button is being held.
+    private Rigidbody2D _projectileCollision; //Collision detection for the projectile fired by the current weapon. Used to move projectiles after firing.
+    private float _bulletSpeed = 800f; //Speed of projectiles fired by current weapon; 800 is default value.
+    private int _weaponHeld = 0; //Weapon currently in use by the player.
 
-    [SerializeField] private float _pickupRange = .5f;
+    [SerializeField] private float _movementSpeed = 500f;
+    [SerializeField] private float _jumpForce = 450f;
 
-    private FacingDirection _direction = FacingDirection.Right;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Initialize access to the main camera and player collision
+        _cam = Camera.main;
+        _playerBoundingBox = GetComponent<CapsuleCollider2D>();
 
-    public FacingDirection Direction{ get => _direction; }
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(_movement * _movementSpeed * Time.deltaTime);
+    }
+
+    private void TryFire()
+    {
+        //Handle the logic to determine what weapon to fire.
+        switch(_weaponHeld)
+        {
+            default:
+                //Get values necessary to spawn projectiles outside of player collision
+                _projectileSpawnModifier = Input.mousePosition - _cam.WorldToScreenPoint(_rb.position);
+                _projectileSpawnModifier.Normalize();
+                _projectileSpawnModifier = _projectileSpawnModifier * _playerBoundingBox.size;
+
+                //Spawn the weapon projectile.
+                GameObject newProjectile = Instantiate(_weaponProjectile, _rb.position + _projectileSpawnModifier, Quaternion.identity);
+
+                //Make the projectile move in the desired direction.
+                _projectileSpawnModifier.Normalize();
+                _projectileCollision = newProjectile.GetComponent<Rigidbody2D>();
+                _projectileCollision.AddForce(_projectileSpawnModifier * _bulletSpeed);
+            
+                //Handle the firing rate.
+                _fireRate = Time.time + 0.01f;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if(_isTriggerHeld && Time.time > _fireRate)
+        {
+            TryFire();
+        }
+    }
+
+    private void OnShoot(InputValue value)
+    {
+        OnShootInput?.Invoke();
+        //If the button is pressed, flip to true. Once the button is released, flip to false.
+        //_isTriggerHeld = !_isTriggerHeld;
+    }
 
     public void OnInteract(InputValue value)
     {
-
         _movement = value.Get<Vector2>();
     }
 
     public void OnJump(InputValue value)
     {
-        _rb.AddForce(Vector2.up * _jumpForce);
+        OnJumpInput?.Invoke();
+        //_rb.AddForce(Vector2.up * _jumpForce);
     }
 }

146d9c5f8dd6332886ca0583ec88a9ee6d5b4a84
Adam <adamsauce99@gmail.com>
2023-11-06 17:08:18 -0600

    Applied changes to code pointed out in review

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Pool;
@@ -10,6 +9,15 @@ using UnityEngine.Pool;
 /// </summary>
 public class CharacterController : MonoBehaviour
 {
+    public CharacterItemHandler _characterItemHandler
+    {
+        get
+        {
+            return GetComponent<CharacterItemHandler>();
+        }
+        private set { }
+    }
+
     private Rigidbody2D _rb;
     private Vector2 _movement;
     private float _fireRate = 0; //Used to control weapon firing speed.
@@ -27,11 +35,6 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float _movementSpeed = 500f;
     [SerializeField] private float _jumpForce = 450f;
     [SerializeField] private float _pickupRange = .5f;
-
-    [SerializeField] private Weapon _currentWeapon;
-    [SerializeField] private GameObject _weaponView;
-
-    private enum FacingDirection{Right,Left}
 
     [SerializeField] private FacingDirection _direction;
 
@@ -65,8 +68,7 @@ public class CharacterController : MonoBehaviour
         {
             _direction = FacingDirection.Left;
         }
-        
-        if(_weaponView != null) {UpdateWeaponView();}
+        _characterItemHandler.UpdateItemView();
     }
 
     public void OnJump(InputValue value)
@@ -76,93 +78,31 @@ public class CharacterController : MonoBehaviour
 
     public void OnInteract(InputValue value)
     {
-        Vector2 rayDir;
-        if(_direction == FacingDirection.Right)
-        {
-            rayDir = Vector2.right;
-        }else{
-            rayDir = Vector2.left;
-        }
         // Use a raycast to check if a pickup is in front of the player
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir,_pickupRange);
-        Debug.Log(hit.collider);
-        //if pickup is in front of player, Pickup the wepon
-        if (hit.collider != null 
-            && hit.collider.GetComponent<PickupObject>() != null)
+        // ternary conditional is used to decide what direction to fire raycast based on _direction
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            (_direction == FacingDirection.Right) ? Vector2.right : Vector2.left,
+            _pickupRange
+        );
+
+        //Debug.Log(hit.collider);
+
+        //if interacable is found, interact with it
+        if (hit.collider != null && hit.collider.TryGetComponent<IInteractible>(out IInteractible interactible))
         {
-            PickupWeapon(hit.collider.gameObject.GetComponent<PickupObject>());
+            interactible.Interaction(this);
         }
     }
 
     public void OnDrop(InputValue value)
     {
-        //drop pickup object in world
-        DropItem();
-
-        //remove weapon from being equiped
-        _currentWeapon = null;
-
-        //remove player sprite of weapon
-        Destroy(_weaponView);
+        _characterItemHandler.DropItem();
 
     }
 
-    public void PickupWeapon(PickupObject obj)
+    public FacingDirection GetDirection()
     {
-        //equip weapon in player
-        _currentWeapon = obj._weapon;
-
-        // create player sprite of wepaon
-        _weaponView = new GameObject(
-            _currentWeapon.name,
-            typeof(SpriteRenderer)
-        );
-
-        //set weaponView as a child of player, and display sprite
-        _weaponView.transform.SetParent(this.transform);
-        _weaponView.GetComponent<SpriteRenderer>().sprite = _currentWeapon._playerSprite;
-
-        UpdateWeaponView();
-        //remove world object
-        Destroy(obj.gameObject);
-    }
-
-    private void DropItem()
-    {
-        Vector2 dropDirection = transform.position;
-
-        //create GameObject
-        GameObject pickup = new GameObject(
-            _currentWeapon.name,
-            typeof(BoxCollider2D),
-            typeof(SpriteRenderer),
-            typeof(PickupObject)
-        );
-        //set position in world
-        switch(_direction)
-        {
-            case FacingDirection.Right: dropDirection.x += 1; break;
-            case FacingDirection.Left: dropDirection.x -= 1; break;
-        }
-
-        pickup.transform.position = dropDirection;
-
-        //setup function stuffs
-        pickup.GetComponent<PickupObject>().InitlizeObject(_currentWeapon);
-    }
-
-    private void UpdateWeaponView()
-    {    
-        Vector3 pos = _currentWeapon._localViewPosition;
-
-        if(_direction == FacingDirection.Right)
-        {   
-            _weaponView.transform.localPosition = pos;
-            _weaponView.transform.localScale = new(1,1);
-            
-        }else{
-            _weaponView.transform.localPosition = new(pos.x*-1,pos.y,0);
-            _weaponView.transform.localScale = new(-1,1);
-        }
+        return _direction;
     }
 }
--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]

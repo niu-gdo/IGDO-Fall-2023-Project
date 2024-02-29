b79cb8cf1612ec63ceda900520890f51c73ec507
AdamBougher <a.bougher1999@gmail.com>
2024-02-29 17:23:13 -0600

    moved logic out of main

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -22,57 +22,9 @@ public class CharacterController : MonoBehaviour
 
     private Vector2 _movement;
 
-    [SerializeField] private float _pickupRange = .5f;
-
-    private FacingDirection _direction = FacingDirection.Right;
-
-    public FacingDirection Direction{ get => _direction; }
-
-    public void OnInteract(InputValue value)
-    {
-        // Use a raycast to check if a pickup is in front of the player
-        // ternary conditional is used to decide what direction to fire raycast based on _direction
-        RaycastHit2D hit = Physics2D.Raycast(
-            transform.position,
-            (_direction == FacingDirection.Right) ? Vector2.right : Vector2.left,
-            _pickupRange
-        );
-
-        //Debug.Log(hit.collider);
-
-        //if interacable is found, interact with it
-        if (hit.collider != null && hit.collider.TryGetComponent<IInteractible>(out IInteractible interactible))
-        {
-            interactible.Interaction(this);
-        }
-    }
-
-    public void OnDrop(InputValue value)
-    {
-        _characterItemHandler.DropItem();
-
-    }
-
-
-    private void ProcessMovement(InputValue value)
-    {
-        _movement = value.Get<Vector2>();
-
-        if (value.Get<Vector2>() == Vector2.right)
-        {
-            _direction = FacingDirection.Right;
-        }
-        else if (value.Get<Vector2>() == Vector2.left)
-        {
-            _direction = FacingDirection.Left;
-        }
-        _characterItemHandler.UpdateItemView();
-    }
-
+ 
 
     public void OnShoot(InputValue value) => OnTriggerHeld?.Invoke(value.isPressed);
-    public void OnMove(InputValue value) => ProcessMovement(value);
-   
 
     public void OnJump(InputValue value) => OnJumpInput?.Invoke();
 }
--- Assets/Scripts/Controllers/CharacterItemHandler.cs
+++ Assets/Scripts/Controllers/CharacterItemHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterItemHandler : MonoBehaviour
 {
@@ -9,6 +10,54 @@ public class CharacterItemHandler : MonoBehaviour
 
     [SerializeField, Tooltip("Child Gameobject of Player that shows currentWeapon's sprite")]
     private GameObject _weaponView;
+
+    [SerializeField] private float _pickupRange = .5f;
+
+    private FacingDirection _direction = FacingDirection.Right;
+
+    public FacingDirection Direction{ get => _direction; }
+
+    public void OnInteract(InputValue value)
+    {
+        // Use a raycast to check if a pickup is in front of the player
+        // ternary conditional is used to decide what direction to fire raycast based on _direction
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            (_direction == FacingDirection.Right) ? Vector2.right : Vector2.left,
+            _pickupRange
+        );
+
+        //Debug.Log(hit.collider);
+
+        //if interacable is found, interact with it
+        if (hit.collider != null && hit.collider.TryGetComponent<IInteractible>(out IInteractible interactible))
+        {
+            interactible.Interaction(GetComponent<CharacterController>());
+        }
+    }
+
+    public void OnDrop(InputValue value)
+    {
+        DropItem();
+
+    }
+
+
+    private void ProcessMovement(InputValue value)
+    {
+        var _movement = value.Get<Vector2>();
+
+        if (value.Get<Vector2>() == Vector2.right)
+        {
+            _direction = FacingDirection.Right;
+        }
+        else if (value.Get<Vector2>() == Vector2.left)
+        {
+            _direction = FacingDirection.Left;
+        }
+
+        UpdateItemView();
+    }
 
 
     public void UpdateItemView()
@@ -20,7 +69,7 @@ public class CharacterItemHandler : MonoBehaviour
 
         Vector3 pos = _currentWeapon._localViewPosition;
 
-        if (GetComponent<CharacterController>().Direction == FacingDirection.Right)
+        if (Direction == FacingDirection.Right)
         {
             _weaponView.transform.localPosition = pos;
             _weaponView.transform.localScale = new(1, 1);
@@ -38,7 +87,7 @@ public class CharacterItemHandler : MonoBehaviour
         //equip weapon in player
         _currentWeapon = weapon;
 
-        // create player sprite of wepaon
+        // create player sprite of weapon
         _weaponView = new GameObject(
             _currentWeapon.name,
             typeof(SpriteRenderer)
@@ -57,7 +106,7 @@ public class CharacterItemHandler : MonoBehaviour
         Vector2 dropDirection = transform.position;
         
         //get position in world to spawn pickup
-        switch (GetComponent<CharacterController>().Direction)
+        switch (Direction)
         {
             case FacingDirection.Right: dropDirection.x += 1; break;
             case FacingDirection.Left: dropDirection.x -= 1; break;
@@ -65,11 +114,14 @@ public class CharacterItemHandler : MonoBehaviour
 
         PickupObject.CreatePickup(dropDirection,_currentWeapon);
 
-        //remove weapon from being equiped
+        //remove weapon from being equipped
         _currentWeapon = null;
 
         //remove player sprite of weapon
         Destroy(_weaponView);
 
     }
+
+    public void OnMove(InputValue value) => ProcessMovement(value);
+   
 }
